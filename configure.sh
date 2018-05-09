e62ecfee3455edc64f218e45b8729fa4ae7a3df4
Daniel Heene <heene@d-sire.com>
2018-05-09 14:05:28 +0200

    fixing string replacement for linux environments

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 for i in "$@"
 do
-    case $i in
+    case ${i} in
         -e=*|--email=*)
         EMAIL="${i#*=}"
         shift
@@ -16,15 +16,19 @@ done
 
 if [ -z ${EMAIL} ]; then echo "missing email parameter"; exit 1 ;fi
 if [ -z ${DOMAIN} ]; then echo "missing domain paramter"; exit 1 ;fi
+if [ `uname` == 'Darwin' ]; then
+    SED_CMD='sed -i "" -e'
+else
+    SED_CMD='sed -i -e'
+fi
 
-echo "copying example files"
+echo "generating files from examples"
 cp -n ./etc/acme.example.json ./etc/acme.json
 cp ./etc/traefik.example.toml ./etc/traefik.toml
 cp docker-compose.example.yml docker-compose.yml
 
-echo "pasting email and domain values to files"
-sed -i "" -e "s/EMAIL_PARAM_HERE/$EMAIL/g" etc/traefik.toml
-sed -i "" -e "s/DOMAIN_PARAM_HERE/$DOMAIN/g" etc/traefik.toml
-sed -i "" -e "s/DOMAIN_PARAM_HERE/$DOMAIN/g" docker-compose.yml
-
+echo "pasting email and domain values"
+eval ${SED_CMD} "s/EMAIL_PARAM_HERE/${EMAIL}/g" etc/traefik.toml
+eval ${SED_CMD} "s/DOMAIN_PARAM_HERE/${DOMAIN}/g" etc/traefik.toml
+eval ${SED_CMD} "s/DOMAIN_PARAM_HERE/${DOMAIN}/g" docker-compose.yml
 exit 0
